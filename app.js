e1e28a72a5953c1841847931d69cb2cc775f1ccb
vuong.khanhtran@gft.com <V*gftithubforthefirstTime>
2022-11-29 14:00:12 +0700

    update

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 'user strict';
 const express = require('express')
+const os = require('os')
+
 const app = express()
 const port = 8080
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World! test trigger build')
+  res.send(
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
+  )
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port} ${os.hostname}`)
 })
